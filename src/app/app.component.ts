4c7c7cd1c820ac56f81a58113378f780d6cc663f
khushboorahman <krahmann98@gmail.com>
2025-05-15 16:16:00 +0530

    Created complete reactive form concept with an example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component, VERSION } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Angular Forms ' + VERSION.full;
+
+  reactiveForm = new FormGroup({
+    FullName : new FormControl('', Validators.required),
+    ContactNum : new FormControl('', Validators.required),
+    Email : new FormControl('', Validators.email),
+    Address : new FormControl('', Validators.required)
+  });
+
+  onSubmit() {
+    console.warn(this.reactiveForm.value);
+  }
 }
